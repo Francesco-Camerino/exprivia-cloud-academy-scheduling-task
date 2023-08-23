0cab6944165332958df508bd406ed1f91aa00701
Francesco Camerino <frcamerino@gmail.com>
2023-08-23 09:50:58 +0200

    final project

--- src/test/java/com/example/schedulingtasks/SchedulingTasksApplicationTests.java
+++ src/test/java/com/example/schedulingtasks/SchedulingTasksApplicationTests.java
@@ -1,13 +1,18 @@
 package com.example.schedulingtasks;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SchedulingTasksApplicationTests {
 
+	@Autowired
+	ScheduledTasks tasks;
 	@Test
 	void contextLoads() {
+		Assertions.assertNotNull(tasks);
 	}
 
 }
